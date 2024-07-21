257dfbc027f2feb9b0115f43dbaffafd4339b605
Pranjal Kushwaha <pranjalkushwahaE130@gmail.com>
2024-07-21 11:43:28 +0530

    Static updated

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1721542153',resources: [
+    cacheName: 'chirpy-1721542396',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
