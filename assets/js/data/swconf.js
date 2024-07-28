9c0b593be95243ed66f80992f88fab94da84bc5f
Pranjal Kushwaha <pranjalkushwahaE130@gmail.com>
2024-07-28 16:08:12 +0530

    Corrected grammar

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722160839',resources: [
+    cacheName: 'chirpy-1722163061',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
