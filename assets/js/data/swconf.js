a49a6edb12430f2e30cbabedd6b15cbaacb17630
Pranjal Kushwaha <pranjalkushwahaE130@gmail.com>
2024-08-08 09:07:00 +0530

    add justify alignment to home

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1723087893',resources: [
+    cacheName: 'chirpy-1723088179',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
