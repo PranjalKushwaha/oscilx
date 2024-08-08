49811564941a9545a9e240f8ad34ecaeed02baa8
Pranjal Kushwaha <pranjalkushwahaE130@gmail.com>
2024-08-08 09:02:05 +0530

    lighten trace image

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1723087185',resources: [
+    cacheName: 'chirpy-1723087893',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
